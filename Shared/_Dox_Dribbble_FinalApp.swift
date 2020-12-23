bebec6da765c801466314a09651ee6c70d94660c
AJ Picard <Contact@saucedesigns.io>
2020-12-23 17:05:18 -0500

    Initial Commit

---
+++ Shared/_Dox_Dribbble_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Dox_Dribbble_FinalApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct _Dox_Dribbble_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
